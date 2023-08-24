bda3c8aa0d2a77faf39900309579e7c1313e400f
galohot <galohot@protonmail.com>
2023-08-25 05:14:08 +0700

    publish

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+
+class AdminController extends Controller
+{
+    public function AdminDashboard(){
+        
+        return view('admin.index');
+
+    } //end method
+
+    public function AdminLogout(Request $request)
+    {
+        Auth::guard('web')->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect('/admin/login');
+    } //end method
+
+    
+    public function AdminLogin(){
+
+        return view('admin.admin_login');
+
+    }//end method
+
+    public function AdminProfile(){
+
+        $id = Auth::user()->id;
+        $profileData = User::find($id);
+        return view('admin.body.admin_profile_view',compact('profileData'));
+
+    }//end method
+
+
+
+}
